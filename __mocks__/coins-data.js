83e3ea336bb2600937309487f78adf1754181365
Calvin Ting <calvinting@protonmail.com>
2018-05-26 10:47:47 -0700

    add coins mockdata

---
+++ __mocks__/coins-data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Nickel: { name: "Nickel", price: 0.05, stock: 100 },
+  Dime: { name: "Dime", value: 0.1, stock: 55 },
+  Quarter: { name: "Quarter", value: 0.25, stock: 41 },
+  Loonie: { name: "Loonie", value: 1.0, stock: 30 },
+  Toonie: { name: "Toonie", value: 2.0, stock: 10 }
+};
