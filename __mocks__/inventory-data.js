a5ce34d99a818ee31bff4b520e1a060738ceff21
Calvin Ting <calvinting@protonmail.com>
2018-05-26 10:42:26 -0700

    add function to stock up according to value and name with tests

--- __mocks__/inventory-data.js
+++ __mocks__/inventory-data.js
@@ -1,8 +1,8 @@
 module.exports = {
-  Redbull: { name: "Redbull", price: 2.0, amount: 10 },
-  Coke: { name: "Coke", price: 1.75, amount: 3 },
-  Vodka: { name: "Vodka", price: 2.5, amount: 0 },
-  Beer: { name: "Beer", price: 1.5, amount: 5 },
-  Orange: { name: "Oranges", price: 1.75, amount: 8 },
-  Coconut: { name: "Coconut", price: 1.75, amount: 2 }
+  Redbull: { name: "Redbull", price: 2.0, stock: 10 },
+  Coke: { name: "Coke", price: 1.75, stock: 3 },
+  Vodka: { name: "Vodka", price: 2.5, stock: 0 },
+  Beer: { name: "Beer", price: 1.5, stock: 5 },
+  Orange: { name: "Oranges", price: 1.75, stock: 8 },
+  Coconut: { name: "Coconut", price: 1.75, stock: 2 }
 };
--- __tests__/vendingMachine.spec.js
+++ __tests__/vendingMachine.spec.js
@@ -10,4 +10,18 @@ describe("vendingMachine", () => {
       expect(result).toEqual(products);
     });
   });
+  describe("when stockUp is called with a stock of 0", () => {
+    it("should return stock up according to the name and amount of stock up", () => {
+      const result = Machine.stockUp(products.Vodka.name, 10);
+      expect(result).toEqual(10);
+    });
+  });
+  describe("when stockUp is called with a stock of 10", () => {
+    it("should return There is x left and it does not need to be restocked", () => {
+      const result = Machine.stockUp(products.Redbull.name, 10);
+      expect(result).toBe(
+        "There is 10 left and it does not need to be restocked."
+      );
+    });
+  });
 });
