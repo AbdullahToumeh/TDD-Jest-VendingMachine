26ae0728935a10161deb1d5a6de95fc47958f27c
Calvin Ting <calvinting@protonmail.com>
2018-05-28 00:04:25 -0700

    add not enough money test suite, coin bank is now updated and inserted into real coin-data when purchase is done, coin-data now updated according to inserted coins

--- __tests__/CVMNotEnoughMoney.spec.js
+++ __tests__/CVMNotEnoughMoney.spec.js
@@ -6,12 +6,21 @@ const coinBank = require("../__mocks__/coin-bank");
 let Machine = new customerVendingMachine(products, coins, coinBank);
 
 describe("customerVendingMachine", () => {
-  describe("when coins are inserted via coinInsert, and product is chosen with master chooseProductVM", () => {
-    it("should return Product with change with coinbank stock cleared", () => {
-      const result2 = Machine.coinInsert("Toonie");
+  describe("when not enough coins are inserted via coinInsert, and product is chosen with master chooseProductVM", () => {
+    it("should return the amount needed for the product", () => {
+      const result1 = Machine.coinInsert("Nickel");
+      const result2 = Machine.coinInsert("Loonie");
       const result3 = Machine.chooseProductVM(products.Beer.name);
+      expect(result3).toBe("You do not have enough money, please put in 0.45");
+    });
+  });
+  describe("when enough coins are inserted via coinInsert, and product is not available but is chosen with master chooseProductVM", () => {
+    it("should product is not in stock", () => {
+      const result1 = Machine.coinInsert("Loonie");
+      const result2 = Machine.coinInsert("Loonie");
+      const result3 = Machine.chooseProductVM(products.Vodka.name);
       expect(result3).toBe(
-        "Here is your Beer. Your change is Quarter:2. There is now 4 left. Thank you."
+        "There is no more stock left, please pick another product"
       );
     });
   });
